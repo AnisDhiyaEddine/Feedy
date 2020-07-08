5d7bea79ae68b54f1c289882dd38bb4ad0adc741
ANISDHIYAEDDINE <dhiaeboudiaf@gmail.com>
2020-07-08 14:28:11 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hello: "world" });
+  res.send({ Hello: "My lovely .." });
 });
 
 const PORT = process.env.PORT || 3000;
