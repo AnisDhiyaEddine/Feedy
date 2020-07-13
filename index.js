c73d364c32de74dac02c5fe83f89074b2572b517
ANISDHIYAEDDINE <dhiaeboudiaf@gmail.com>
2020-07-13 15:41:40 +0100

    hot fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
 
   let path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
